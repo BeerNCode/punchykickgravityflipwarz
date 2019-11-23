a093702f201ff369f7981501bc54977da9528a02
Alexander Kent <alexander@lap-pop.lan>
2019-11-23 11:22:49 +0000

    perlin world generation

--- punchykickgravityflipwarz/world.py
+++ punchykickgravityflipwarz/world.py
@@ -9,6 +9,9 @@ from punchykickgravityflipwarz.entity import Entity
 from punchykickgravityflipwarz.vector import Vector
 from punchykickgravityflipwarz.tile import Tile
 
+import noise 
+import numpy as np
+
 imageFile = os.path.join("punchykickgravityflipwarz", "resources", "world.png")
 
 logger = logging.getLogger(__name__)
@@ -17,6 +20,12 @@ TILE_SIZE = 16
 WIDTH = 1024
 HEIGHT = 768
 
+SHAPE = (WIDTH,HEIGHT)
+SCALE = 10.0
+OCTAVES = 6
+PERSISTANCE = 0.5
+LACUNARITY = 2.0
+
 tile_sprites = SpriteSheet(os.path.join('punchykickgravityflipwarz', 'resources', "block.jpg"))
 
 class World:
@@ -24,8 +33,9 @@ class World:
     def __init__(self):
         self.tiles = pygame.sprite.Group()
         self.scale = 8
+        self.noisy_world = np.zeros(SHAPE)
 
-        if True: # random world
+        if False: # random world
             for i in range(0, 50):
                 platform_x = randint(0, (WIDTH / TILE_SIZE))
                 platform_y = randint(0, HEIGHT / TILE_SIZE)
@@ -33,7 +43,7 @@ class World:
                 for x in range(0, platform_width):
                     tile = Tile((platform_x + x) * TILE_SIZE, platform_y * TILE_SIZE)
                     self.tiles.add(tile)
-        else:
+        elif False:
             for x in range(0,10):    
                 tile = Tile(x * TILE_SIZE, HEIGHT - 6 * TILE_SIZE)
                 self.tiles.add(tile)
@@ -41,6 +51,15 @@ class World:
         for x in range(0, int(math.floor(WIDTH / TILE_SIZE))):
             tile = Tile(x * TILE_SIZE, HEIGHT - 2 * TILE_SIZE)
             self.tiles.add(tile)
+
+        self.noisy_world = self.generate_noise(SHAPE, SCALE, OCTAVES, PERSISTANCE, LACUNARITY)
+
+        for i in range(0, int(math.floor(SHAPE[0]/TILE_SIZE))):
+            for j in range(0, int(math.floor(SHAPE[1]/TILE_SIZE))):
+                if (self.noisy_world[i][j] > 0.1):
+                    tile = Tile(i * TILE_SIZE, j * TILE_SIZE)
+                    self.tiles.add(tile)
+        print(len(self.tiles))
 
         self.surface = Surface((WIDTH, HEIGHT))
         self.tiles.draw(self.surface)
@@ -67,5 +86,18 @@ class World:
 
     def get_index(self, x, y, width):
         return y * width + x
+
+    def generate_noise(self, shape, scale, octaves, persistance, lacunarity):
+        for i in range (WIDTH):
+            for j in range (HEIGHT):
+                self.noisy_world[i][j] = noise.pnoise2(i/scale, j/scale, octaves=octaves, 
+                                    persistence=PERSISTANCE, 
+                                    lacunarity=LACUNARITY, 
+                                    repeatx=WIDTH, 
+                                    repeaty=HEIGHT, 
+                                    base=0)
+                #print(self.noisy_world[i][j])
+        return self.noisy_world
+
 
 
